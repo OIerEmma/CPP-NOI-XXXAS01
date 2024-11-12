2824d7e88e3e3035065902b0eefc7e3f80500ea7
Emme <gmx20140823@163.com>
2024-11-12 20:15:07 +0800

    question1006-1008

---
+++ src/part01/ch02/exercise/question1006.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Emma on 2024/11/12.
+//
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    cout << a + b << endl;
+    return 0;
+}
