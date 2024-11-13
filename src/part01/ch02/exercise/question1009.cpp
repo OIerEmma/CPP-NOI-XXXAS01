0c0bf2ad71f8be7543ae881159fffad2405e485d
Emme <gmx20140823@163.com>
2024-11-13 17:53:09 +0800

    question1009-1010

---
+++ src/part01/ch02/exercise/question1009.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Emma on 2024/11/13.
+//
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b, x, y;
+    cin >> x >> y;
+    a = x / y;
+    b = x % y;
+    cout << a << " " << b << endl;
+    return 0;
+}
---
+++ src/part01/ch02/exercise/question1010.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Emma on 2024/11/13.
+//
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    double x, a, b;
+    cin >> a >> b;
+    x = a / b;
+    printf("%0.9lf\n", x);
+}
