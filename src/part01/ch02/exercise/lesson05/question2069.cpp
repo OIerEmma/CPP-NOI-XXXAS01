f9c876bfdd297541d0ac256bf2ec1cf984015362
Emme <gmx20140823@163.com>
2025-01-01 22:35:46 +0800

    adjust question2069

--- src/part01/ch02/exercise/lesson05/question2069.cpp
+++ src/part01/ch02/exercise/lesson05/question2069.cpp
@@ -7,12 +7,12 @@ using namespace std;
 
 int main() {
     long long a, b, c, d, e;
-    scanf("%ld %ld %ld %ld %ld", &a, &b, &c, &d, &e);
+    scanf("%lld %lld %lld %lld %lld", &a, &b, &c, &d, &e);
     a = a / 3; b = b + a; e = e + a;
     b = b / 3; c = c + b; a = a + b;
     c = c / 3; d = d + c; b = b + c;
     d = d / 3; e = e + d; c = c + d;
     e = e / 3; a = a + e; d = d + e;
-    printf("%5ld%5ld%5ld%5ld%5ld\n", a, b, c, d, e);
+    printf("%5lld%5lld%5lld%5lld%5lld\n", a, b, c, d, e);
     return 0;
 }
