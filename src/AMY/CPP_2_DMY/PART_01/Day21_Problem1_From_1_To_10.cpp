12f78153e6ca73c8545650c44cba839fc22009b6
Emme <gmx20140823@163.com>
2025-09-04 22:21:06 +0800

    Amy added DMY day21 problems

---
+++ src/AMY/CPP_2_DMY/PART_01/Day21_Problem1_From_1_To_10.cpp
@@ -0,0 +1,12 @@
+//
+// Created by Emma on 2025/9/3.
+//
+#include<iostream>
+using namespace std;
+
+int main() {
+    for (int i = 10; i >= 1; i--) {
+        cout << i << endl;
+    }
+    return 0;
+}
---
+++ src/AMY/CPP_2_DMY/PART_01/Day21_Problem2_Print_a_To_b.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Emma on 2025/9/3.
+//
+#include<iostream>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    for (int i = a; i <= b; i++) {
+        cout << i << endl;
+    }
+    return 0;
+}
