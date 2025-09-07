9608116a8bf8a79de046b47b5801de79c6c47f06
Emme <gmx20140823@163.com>
2025-09-07 08:36:07 +0800

    add file

--- src/part03/exercise/ch02/question_1360B.cpp
+++ src/part03/exercise/ch02/question_1360B.cpp
@@ -11,15 +11,22 @@ int k[210], q[420], f[210], a[210];
 int front, rear;
 
 void jump(const int from_f, const int grids) {
+    // 计算上跳或下跳到哪一层
     const int to_f = from_f + grids;
+    // 目标楼层合法性判断
+    if (to_f < 1 || to_f > n) {
+        return;
+    }
+    // 跳转次数加1
+    a[to_f] = a[from_f] + 1;
     if (to_f == target_f) {
         // 到达目标层 计算最小步数
-        minv = min(minv, a[from_f]+1);
-    } else if (to_f >= 1 && to_f <= n && !f[to_f]) {
+        minv = min(minv, a[to_f]);
+    } else if (!f[to_f]) {
         // 到达满足条件的层数则入队
         q[rear++] = to_f;
+        // 更新f[to_f]为true 代表此楼层我已经走过了
         f[to_f] = true;
-        a[to_f] = a[from_f] + 1;
     }
 }
 
@@ -47,8 +54,10 @@ int main() {
     bfs();
     // output
     if (init_f == target_f) {
+        // 如果初始楼层等于目标楼层 不需要走了 输出0
         cout << 0 << endl;
     } else if (minv < STEPS) {
+        // 如果minv被更新过了(就是不等于初始值了) 输出minv
         cout << minv << endl;
     } else {
         cout << -1 << endl;
