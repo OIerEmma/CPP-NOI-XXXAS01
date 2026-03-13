f6771592b763c91e05bce164cc41d62e47e89dc8
Emme.Kwok <emme@EmmeKwoks-Mac-Mini.local>
2026-03-13 22:39:32 +0800

    add GESP code

---
+++ src/EMME/GESP_exam_question/L5_question/202506/L5_202506_question_02_2.cpp
@@ -0,0 +1,33 @@
+//
+// Created by Emme.Kwok on 2026/3/13.
+//
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+
+const int N = 1e5 + 5;
+
+int n, q, a[N], g;
+
+int gcd(int a, int b) {
+    if (a == 0 || b == 0) {
+        return a + b;
+    }
+    return gcd(b, a % b);
+}
+
+int main() {
+    scanf("%d%d", &n, &q);
+    for (int i = 1; i <=n; i++) {
+        scanf("%d", &a[i]);
+    }
+    sort(a + 1, a + n + 1);
+    for (int i = 2; i <= n; i++) {
+        g = gcd(g, a[i] - a[i - 1]);
+    }
+    for (int i = 1; i <= q; i++) {
+        printf("%d\n", gcd(g, a[1] + i));
+    }
+    return 0;
+}
--- src/EMME/GESP_exam_question/L5_question/202509/L5_202509_question_02.cpp
+++ src/EMME/GESP_exam_question/L5_question/202509/L5_202509_question_02.cpp
@@ -1,3 +1,40 @@
 //
 // Created by Emme.Kwok on 2026/2/26.
 //
+#include<iostream>
+using namespace std;
+
+long long l, r, b;
+long long ans = 0;
+const int v[9] = {0, 1, 3, 3, 7, 7, 7, 14, 22};
+
+int check(long long num) {
+    int cnt = 0;
+    while(num) {
+        if (num & 1) {
+            cnt++;
+        }
+        num >>= 1;
+    }
+    return cnt;
+}
+
+long long work(long long n) {
+    if (n <= 8) {
+        return v[n];
+    }
+    b = n >> 2;
+    ans = (b * b << 2) - b;
+    for (long long i = b << 2; i <= n; i++) {
+        if (check(i) % 2) {
+            ans += i;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    cin >> l >> r;
+    cout << work(r) - work(l - 1) << endl;
+    return 0;
+}
