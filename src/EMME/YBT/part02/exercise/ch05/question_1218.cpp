14bc9fe6377aaba55e09a42b178976bbcdd292d0
Emme.Kwok <emme@EmmeKwoks-Mac-Mini.local>
2026-03-16 22:30:02 +0800

    Emme add code

--- src/EMME/YBT/part02/exercise/ch05/question_1218.cpp
+++ src/EMME/YBT/part02/exercise/ch05/question_1218.cpp
@@ -4,41 +4,19 @@
 #include<iostream>
 using namespace std;
 
-int a, b;
-bool isfinish = true;
+int a, b, turn;
 
-void dfs(int na, int nb, bool flag) { // true:first false:second
-    if (!isfinish) return;
+void dfs(int na, int nb) { // true:first false:second
     if (na == 0 || nb == 0) {
-        if (flag) cout << "win" << endl;
-        else cout << "lose" << endl;
-        isfinish = false;
+        turn++;
         return;
     }
-    if (na >= nb) {
-        if (na / nb >= 2) {
-            if (flag) cout << "win" << endl;
-            else cout << "lose" << endl;
-            isfinish = false;
-            return;
-        }
-        dfs(na, nb, !flag);
-    } else {
-        if (nb / na >= 2) cout << "lose" << endl;
-        else dfs(na, nb, true);
+    if (na / nb >= 2 || na == nb) {
+        turn++;
+        return;
     }
-    if (na > nb) {
-        for (int i = 1; i <= na; i++) {
-            if (i % nb == 0) dfs(na, nb - i, !flag);
-        }
-    } else if (na < nb) {
-        for (int i = 1; i <= nb; i++) {
-            if (i % na == 0) dfs(na - i, nb, !flag);
-        }
-    } else {
-        if (flag) cout << "win" << endl;
-        else cout << "lose" << endl;
-    }
+    turn++;
+    dfs(nb, na - nb);
 }
 
 int main() {
@@ -47,15 +25,18 @@ int main() {
         if (a == 0 && b == 0) {
             break;
         }
-        if (a >= b) {
-            if (a / b >= 2) cout << "win" << endl;
-            else dfs(a, b, true);
-        } else {
-            if (b / a >= 2) cout << "lose" << endl;
-            else dfs(a, b, true);
+        int maxab = max(a, b), minab = min(a, b);
+        if (maxab / minab >= 2 || maxab == minab) {
+            cout << "win" << endl;
+            continue;
         }
-        isfinish = true;
-        dfs(a, b, true);
+        turn = 0;
+        dfs(maxab, minab);
+        if (turn % 2 != 0) {
+            cout << "win" << endl;
+        } else {
+            cout << "lose" << endl;
+        }
     }
     return 0;
 }
