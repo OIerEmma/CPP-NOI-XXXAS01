bacb985f959f4d7173b1cfd56b7c339abfa34f50
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-03-20 13:53:24 +0800

    Geek add code YBT CH08 EP8_3

--- src/GEEK/YBT/PART2/CH08/EP8_3_Chessboard_Walk.cpp
+++ src/GEEK/YBT/PART2/CH08/EP8_3_Chessboard_Walk.cpp
@@ -4,9 +4,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int dx[] = {-1, -2, -2};
-int dy[] = {-2, -1, -2};
-int ax, ay, bx, by, vis[101][101];
+#define N 101
+int dx[] = {-1, -2, -1, -2, 1, 2, 1, 2, -2, 2, 2, -2};
+int dy[] = {-2, -1, 2, 1, 2, 1, -2, -1, -2, 2, -2, 2};
+int ax, ay, bx, by, vis[N + 1][N + 1];
 struct node {int x, y;};
 
 int bfs(int x, int y) {
@@ -16,16 +17,17 @@ int bfs(int x, int y) {
     while (!q.empty()) {
         node now = q.front();
         q.pop();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 12; i++) {
             int xx = now.x + dx[i], yy = now.y + dy[i];
-            if (xx < 1 || yy < 1 || vis[xx][yy]) continue;
-            vis[xx][yy] = vis[x][y] + 1;
+            if (xx < 1 || xx > N || yy < 1 || yy > N || vis[xx][yy]) continue;
+            vis[xx][yy] = vis[now.x][now.y] + 1;
             if (xx == 1 && yy == 1) {
                 return vis[xx][yy];
             }
             q.push({xx, yy});
         }
     }
+    return -1;
 }
 
 int main() {
