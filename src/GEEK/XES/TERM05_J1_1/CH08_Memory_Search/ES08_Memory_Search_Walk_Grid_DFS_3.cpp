cd2cb0d55e1fe0758060491e228f4f735d3a0e24
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-03-21 00:23:02 +0800

    Geek add code XES TERM05 CH08 ES23

--- src/GEEK/XES/TERM05_J1_1/CH08_Memory_Search/ES08_Memory_Search_Walk_Grid_DFS_3.cpp
+++ src/GEEK/XES/TERM05_J1_1/CH08_Memory_Search/ES08_Memory_Search_Walk_Grid_DFS_3.cpp
@@ -8,35 +8,34 @@ using namespace std;
 #define MOD 998244353
 
 int n, m, mp[N][N], s[N][N];
-bool vis[N][N];
 
 int dfs(int x, int y) {
+    if (s[x][y] != -1) return s[x][y];
     if (x == n && y == m) return 1;
     if (mp[x][y] == 0) return 0;
-    if (s[x][y]) return s[x][y];
     long long cnt = 0;
     for (int i = 1; i <= mp[x][y]; i++) {
         int xx = x + i, yy = y;
-        if (xx > n || yy > m || mp[xx][yy] == 0 || vis[xx][yy]) continue;
-        vis[xx][yy] = true;
+        if (xx > n || yy > m || mp[xx][yy] == 0) continue;
         cnt += dfs(xx, yy);
-        vis[xx][yy] = false;
     }
     for (int j = 1; j <= mp[x][y]; j++) {
         int xx = x, yy = y + j;
-        if (xx > n || yy > m || mp[xx][yy] == 0 || vis[xx][yy]) continue;
-        vis[xx][yy] = true;
+        if (xx > n || yy > m || mp[xx][yy] == 0) continue;
         cnt += dfs(xx, yy);
-        vis[xx][yy] = false;
     }
     return s[x][y] = int(cnt % MOD);
 }
 
+/**
+ * AC.
+ */
 int main() {
     cin >> n >> m;
     for (int i = 1; i <= n; i++)
         for (int j = 1; j <= m; j++)
             scanf("%d", &mp[i][j]);
+    memset(s, -1, sizeof s);
     printf("%d\n", dfs(1, 1));
     return 0;
 }
--- src/GEEK/XES/TERM05_J1_1/CH08_Winter_Homework/ES23_Save_OIBH_Base.cpp
+++ src/GEEK/XES/TERM05_J1_1/CH08_Winter_Homework/ES23_Save_OIBH_Base.cpp
@@ -7,18 +7,14 @@ using namespace std;
 
 int dx[] = {1, -1, 0, 0};
 int dy[] = {0, 0, 1, -1};
-int n, m, cnt;
-bool vis[N][N];
+int n, m, cnt, vis[N][N];
 char mp[N][N];
 
 void dfs(int x, int y) {
-    if (mp[x][y] == '0' && (x < 2 || mp[x-1][y] == '*') && (x > n - 1 || mp[x+1][y] == '*') && ( y > m - 1 || mp[x][y+1] == '*') && (y < 2 || mp[x][y-1] == '*')) {
-        cnt++;
-    }
+    vis[x][y] = cnt;
     for (int i = 0; i < 4; i++) {
         int xx = x + dx[i], yy = y + dy[i];
         if (xx < 1 || xx > n || yy < 1 || yy > m || mp[xx][yy] == '*' || vis[xx][yy]) continue;
-        vis[xx][yy] = true;
         dfs(xx, yy);
     }
 }
@@ -30,7 +26,39 @@ int main() {
             cin >> mp[i][j];
         }
     }
-    dfs(1, 1);
-    cout << cnt << endl;
+
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i][1] && mp[i][1] == '0') {
+            cnt++;
+            dfs(i, 1);
+        }
+        if (!vis[i][m] && mp[i][m] == '0') {
+            cnt++;
+            dfs(i, m);
+        }
+        if (!vis[1][i] && mp[1][i] == '0') {
+            cnt++;
+            dfs(1, i);
+        }
+        if (!vis[m][i] && mp[m][i] == '0') {
+            cnt++;
+            dfs(m, i);
+        }
+    }
+
+    // for (int i = 1; i <= n; i++) {
+    //     for (int j = 1; j <= m; j++) {
+    //         cout << vis[i][j] << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    int ans = 0;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            if (mp[i][j] == '0' && !vis[i][j]) ans++;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
