eebed2b95bce5805a91bfc244b83bb128cb4510d
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-03-21 23:55:33 +0800

    Geek add code XES TERM05 CH08_HW ES27

---
+++ src/GEEK/XES/TERM05_J1_1/CH08_Winter_Homework/ES27_The_Number_Count.cpp
@@ -0,0 +1,24 @@
+//
+// Created by Geek.Kwok on 2026/3/21.
+//
+#include <bits/stdc++.h>
+using namespace std;
+
+int f[3005];
+
+int dfs(int x) {
+    if (f[x]) return f[x];
+    if (x == 1) return 1;
+    int ans = 1;
+    for (int i = 1; i <= x/2; i++) {
+        ans += dfs(i);
+    }
+    return f[x] = ans;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << dfs(n) << endl;
+    return 0;
+}
