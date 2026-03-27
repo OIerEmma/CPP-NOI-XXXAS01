78114ada4e100b8ab7f870d8f3df68e42282ec31
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-03-27 22:54:18 +0800

    Geek add code LBLD CH01 L01

--- src/GEEK/LBLD/CH01_ARRAY/L01_CYCLEARRAY/CycleArray.cpp
+++ src/GEEK/LBLD/CH01_ARRAY/L01_CYCLEARRAY/CycleArray.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <stdexcept>
 #include <vector>
-#include <ostream>
 using namespace std;
 
 template<typename T>
@@ -28,7 +27,7 @@ class CycleArray {
 
 public:
     CycleArray() : CycleArray(1) {}
-    explicit CycleArray(int size) : arr(size), start(0), end(0), count(1) {}
+    explicit CycleArray(int size) : arr(size), start(0), end(0), count(0) {}
 
     // 在数组头部添加元素，时间复杂度O(1)
     void addFirst(const T &val) {
@@ -51,7 +50,7 @@ public:
         arr[start] = T();
         start = (start + 1) % arr.size();
         count--;
-        // 如果数组元素数量减少到原大小的四分之一，则减少数组大小为一半
+        // 缩容：如果数组元素数量减少到原大小的四分之一，则减少数组大小为一半
         if (count > 0 && count == arr.size() / 4) {
             resize(arr.size() / 2);
         }
@@ -69,5 +68,71 @@ public:
         count++;
     }
 
-    // 删除数组尾部元素，
+    // 删除数组尾部元素，时间复杂度O(1)
+    void removeLast() {
+        if (isEmpty()) {
+            throw runtime_error("Array is empty");
+        }
+        // 因为 end 是开区间，所以先左移，再赋值
+        end = (end - 1 + arr.size()) % arr.size();
+        arr[end] = T();
+        count--;
+        // 缩容：如果数组元素数量减少到原来大小的四分之一，则减少数组大小为一半
+        if (count > 0 && count == arr.size() / 4) {
+            resize(arr.size() / 2);
+        }
+    }
+
+    // 获取数组头部元素，时间复杂度O(1)
+    T getFirst() const {
+        if (isEmpty()) {
+            throw runtime_error("Array is empty");
+        }
+        return arr[start];
+    }
+
+    // 获取数组尾部元素，时间复杂度O(1)
+    T getLast() const {
+        if (isEmpty()) {
+            throw runtime_error("Array is empty");
+        }
+        // end 是开区间，指向的是下一个元素的位置，所以要减 1
+        return arr[(end - 1 + arr.size()) % arr.size()];
+    }
+
+    bool isFull() const {
+        return count == arr.size();
+    }
+
+    bool isEmpty() const {
+        return count == 0;
+    }
+
+    int size() const {
+        return count;
+    }
+
+    void display() const {
+        if (isEmpty()) return;
+        for (int i = 0; i < count; i++) {
+            cout << arr[(start + i) % arr.size()] << " ";
+        }
+        cout << endl;
+    }
 };
+
+int main() {
+    CycleArray<int> carr;
+    carr.addLast(1);
+    carr.addLast(2);
+    carr.addFirst(3);
+    carr.addFirst(4);
+    carr.display();
+
+    carr.removeFirst();
+    carr.display();
+    carr.removeLast();
+    carr.display();
+
+    return 0;
+}
