0ef5d63843d3af81bf8a317749164d0c5b20149c
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-05-16 09:47:53 +0800

    Geek add code 专题系列-DP-01线性DP-05LIS问题-07多状态DP

---
+++ src/GEEK/专题系列/DP/01线性DP/05LIS问题/07多状态DP/LC0376_wiggle-subsequence.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Geek.Kwok on 2026/5/16.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int wiggleMaxLength(vector<int>& nums) {
+        // 状态定义
+        // dp[i][0] 以i为结尾且为正的最长摆动序列长度
+        // dp[i][1] 以i为结尾且为负的最长摆动序列长度
+        // dp[i][0] = max{dp[j][1]} + 1 ( j < i, nums[i] - nums[j] > 0)
+        // dp[i][1] = max{dp[j][0]} + 1 ( j < i, nums[i] - nums[j] < 0)
+        // 初始化
+        int n = nums.size(), ans0 = 1, ans1 = 1;
+        vector<vector<int>> dp(n, vector<int>(2, 1));
+        // 计算顺序
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] - nums[j] > 0) {
+                    dp[i][0] = max(dp[i][0], dp[j][1] + 1);
+                    dp[i][1] = max(dp[i][1], dp[j][1]);
+                } else if (nums[i] - nums[j] < 0) {
+                    dp[i][0] = max(dp[i][0], dp[j][0]);
+                    dp[i][1] = max(dp[i][1], dp[j][0] + 1);
+                } else {
+                    dp[i][0] = max(dp[i][0], dp[j][0]);
+                    dp[i][1] = max(dp[i][1], dp[j][1]);
+                }
+            }
+            ans0 = max(ans0, dp[i][0]);
+            ans1 = max(ans1, dp[i][1]);
+        }
+        // 返回结果
+        return max(ans0, ans1);
+    }
+};
