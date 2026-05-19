050307a2a26dcfa062b14f8427bf67bb14d731f4
zhengqi.guo <zhengqi.guo@leyantech.com>
2026-05-19 19:19:34 +0800

    Geek add code

---
+++ src/GEEK/专题系列/DP/01线性DP/06游戏问题/01基础巩固/LC0377_combination-sum-iv.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Geek.Kwok on 5/19/26.
+//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) {
+        // 完全背包问题求方案数
+        // 状态定义：dp[j] 前i个数的总和恰好为j的总方案数
+        // 初始化
+        vector<unsigned int> dp(target + 1, 0);
+        dp[0] = 1;
+        // 计算顺序
+        for (int i = 1; i <= target; i++)
+            for (int num: nums)
+                if (i >= num)
+                    dp[i] += dp[i - num];
+        // 返回结果
+        return dp[target];
+    }
+
+    int combinationSum4_2(vector<int>& nums, int target) {
+        int n = nums.size();
+        // 完全背包问题求方案数
+        // 状态定义：dp[i][j] 前i个数的总和恰好为j的总方案数
+        // 初始化
+        vector<vector<unsigned int>> dp(n + 1, vector<unsigned int>(target + 1, 0));
+        dp[0][0] = 1;
+        // 计算顺序
+        // for (int i = 1; i <= target; i++)
+        //     for (int num: nums)
+        //         if (i >= num)
+        //             dp[i] += dp[i - num];
+        for (int i = 1; i <= n; i++)
+            for (int j = 0; j <= target; j++) {
+                int maxK = j / nums[i - 1];
+                for (int k = 0; k <= maxK; k++)
+                    dp[i][j] += dp[i - 1][j - k * nums[i-1]];
+            }
+        // 返回结果
+        return dp[n][target];
+    }
+};
+
+int main() {
+    return 0;
+}
