2ebffa586beb752cac8a4190c22be999d76609b3
Emme.Kwok <gmx20140823@163.com>
2026-06-26 20:16:09 +0800

    Emme add code

---
+++ src/EMME/GESP_exam_question/L6_question/202412/L6_202412_question_02.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Emme.Kwok on 2026/6/26.
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+struct car {
+    int a, b;
+};
+
+struct sta {
+    int
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n, m, x;
+    cin >> n >> m >> x;
+    vector<int> a(n), b(m);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    for (int i = 0; i < m; i++) cin >> b[i];
+    for (int i = 0, j = ; )
+    return 0;
+}
