af027218481bbe878433fe38c2db199482e56fe3
Emme.Kwok <gmx20140823@163.com>
2026-07-01 21:35:44 +0800

    Emme add code

--- src/EMME/GESP_exam_question/L6_question/202412/L6_202412_question_02.cpp
+++ src/EMME/GESP_exam_question/L6_question/202412/L6_202412_question_02.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 
 struct car {
-    int a, b;
+    int a, b, diff;
 };
 
-struct sta {
-    int
+struct site {
+    int p, c;
 };
 
 int main() {
@@ -17,9 +17,28 @@ int main() {
     cin.tie(nullptr);
     int n, m, x;
     cin >> n >> m >> x;
-    vector<int> a(n), b(m);
-    for (int i = 0; i < n; i++) cin >> a[i];
-    for (int i = 0; i < m; i++) cin >> b[i];
-    for (int i = 0, j = ; )
+    vector<site> s(n);
+    vector<car> c(m);
+    for (int i = 0; i < n; i++) cin >> s[i].p >> s[i].c;
+    for (int i = 0; i < m; i++) cin >> c[i].a >> c[i].b, c[i].diff = c[i].a - c[i].b;
+    sort(s.begin(), s.end(), [](const site &a, const site &b) { return a.p < b.p; });
+    sort(c.begin(), c.end(), [](const car &a, const car &b){ return a.diff > b.diff; });
+    long long ans = 0;
+    int i = 0, j = m - 1, s_front = 0, s_back = n - 1;
+    while (i <= j) {
+        while (c[i].diff >= 0) {
+            if (s[s_front].c <= 0) s_front++;
+            s[s_front].c--;
+            ans += (2LL * x * c[i].b + 2LL * s[s_front].p * c[i].diff);
+            i++;
+        }
+        while (c[j].diff < 0) {
+            if (s[s_back].c <= 0) s_back--;
+            s[s_back].c--;
+            ans += (2LL * x * c[j].a + 2LL * (x - s[s_back].p) * (-c[j].diff));
+            j--;
+        }
+    }
+    cout << ans << endl;
     return 0;
 }
---
+++ src/EMME/GESP_exam_question/L6_question/202503/L6_202503_question_02.cpp
@@ -0,0 +1,27 @@
+//
+// Created by Emme.Kwok on 2026/6/26.
+//
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;
+    cin >> n;
+    vector<int> a(2 * n + 1);
+    for (int i = 1; i <= n; i++) cin >> a[i], a[i + n] = a[i];
+    vector<long long> pre(2 * n + 1, 0);
+    for (int i = 1; i <= 2 * n; i++) pre[i] = pre[i - 1] + a[i];
+    deque<int> q;
+    q.push_back(0);
+    long long ans = -1e18;
+    for (int i = 1; i <= 2 * n; i++) {
+        while (!q.empty() && q.front() < i - n) q.pop_front();
+        ans = max(ans, pre[i] - pre[q.front()]);
+        while (!q.empty() && pre[i] < pre[q.back()]) q.pop_back();
+        q.push_back(i);
+    }
+    cout << ans << endl;
+    return 0;
+}
